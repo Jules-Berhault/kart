a5a59df1e48d0f70a83c8b638fbf2ae2a539905e
Paul-antoineLeTolguenec <paul-antoine.le_tolguenec@ensta-bretagne.org>
2020-02-28 09:35:17 +0100

    camera update

--- src/camera_node.py
+++ src/camera_node.py
@@ -3,11 +3,12 @@
 import numpy as np
 import cv2
 import sys
-from mrpiZ_lib import *
+""" from mrpiZ_lib import *
+ """
 import rospy
 from std_msgs.msg import Float64
 
-
+dt=0.1
 
 """ initialisation du node """
 rospy.init_node('camera_node', anonymous=True)
@@ -26,33 +27,42 @@ video_capture.set(3, WIDTH)
 video_capture.set(4, HEIGHT)
 rate = rospy.Rate(1/dt) # 10hz
 
-while not rospy.is_shutdown():
-    # Capture the frames
-    ret, frame = video_capture.read()
-    # Crop the image
-    # Keep the 100 lower pixels
-    crop_img = frame[379:480, 0:640]
-    # Convert to grayscale
-    gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-    # Gaussian blur
-    blur = cv2.GaussianBlur(gray,(5,5),0)
-    # Color thresholding
-    ret,thresh = cv2.threshold(blur,60,255,cv2.THRESH_BINARY_INV)
-    # Find the contours of the frame
-    contours,hierarchy = cv2.findContours(thresh.copy(), 1, cv2.CHAIN_APPROX_NONE)
+""" while not rospy.is_shutdown(): """
+# Capture the frames
+ret, frame = video_capture.read()
+# Crop the image
+# Keep the 100 lower pixels
+crop_img = frame[379:480, 0:640]
+
+# Convert to grayscale
+gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+
+# Gaussian blur
+blur = cv2.GaussianBlur(gray,(5,5),0)
+
+# Color thresholding
+ret,thresh = cv2.threshold(blur,60,255,cv2.THRESH_BINARY_INV)
+cv2.imshow("frame", crop_img)
+# Find the contours of the frame
+
+result = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+contours, hierarchy = result if len(result) == 2 else result[1:3]
 
 
-    # Find the biggest contour (if detected)
-    if len(contours) > 0:
-        c = max(contours, key=cv2.contourArea)
-        M = cv2.moments(c)
 
-        # Skip to avoid div by zero
-        if int(M['m00']) == 0:
-            continue
+# Find the biggest contour (if detected)
+if len(contours) > 0:
+    c = max(contours, key=cv2.contourArea)
+    M = cv2.moments(c)
 
-    # Get the line center
-    cx = int(M['m10']/M['m00'])
-    cy = int(M['m01']/M['m00'])
-    rate.sleep()
+    # Skip to avoid div by zero
+    """ if int(M['m00']) == 0:
+        continue """
+
+# Get the line center
+cx = int(M['m10']/M['m00'])
+cy = int(M['m01']/M['m00'])
+   
+""" rate.sleep() """
 
