a09e2b4fafe3efaf11817ef178322f5ccfe755bd
Teusner <quentin.brateau@ensta-bretagne.org>
2020-03-29 14:47:32 +0200

    Changing pwm command from float to twist in order to use it with keyteleop

--- src/pwm.py
+++ src/pwm.py
@@ -2,16 +2,22 @@
 
 import rospy
 import numpy as np
-from std_msgs.msg import Float64
+from geometry_msgs.msg import Twist
 
 
-def u1_callback(cmd):
-    global u1
-    u1 = int(500000 * cmd.data) + 1500000
+def cmd_callback(cmd):
+	global u1
+	global u2
+	u1 = int(500000 * cmd.linear.x) + 1500000
+	u2 = int(500000 * cmd.angular.z) + 1500000
 
-def u2_callback(cmd):
-    global u2
-    u2 = int(500000 * cmd.data) + 1500000
+# def u1_callback(cmd):
+#     global u1
+#     u1 = int(500000 * cmd.data) + 1500000
+
+# def u2_callback(cmd):
+#     global u2
+#     u2 = int(500000 * cmd.data) + 1500000
 
 if __name__ == "__main__":
     # Command variables
@@ -19,8 +25,9 @@ if __name__ == "__main__":
 
     # ROS
     rospy.init_node('driver_node')
-    rospy.Subscriber("u1", Float64, u1_callback)
-    rospy.Subscriber("u2", Float64, u2_callback)
+    rospy.Subscriber("cmd_vel", Twist, cmd_callback)
+    # rospy.Subscriber("u1", Float64, u1_callback)
+    # rospy.Subscriber("u2", Float64, u2_callback)
 
     r = rospy.Rate(20) # 20hz
 
