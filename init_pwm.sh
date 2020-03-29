353c233c7dd0f47a33c81e6fd5e05ec8621e181e
mchouch <quentin.brateau@ensta-bretagne.org>
2020-03-29 14:22:38 +0200

    Update

--- init_pwm.sh
+++ init_pwm.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-set -uo pipefail
-trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
-IFS=$'\n\t'
+#! /bin/sh
 
 chip0=/sys/class/pwm/pwmchip0
 pwm0=$chip0/pwm0
@@ -9,9 +6,17 @@ pwm1=$chip0/pwm1
 
 echo 0 > $chip0/export
 echo 1 > $chip0/export
+
 echo 20000000  > $pwm0/period
 echo 20000000  > $pwm1/period
+
 echo 1500000 > $pwm0/duty_cycle
 echo 1500000 > $pwm1/duty_cycle
+
 echo 1 > $pwm0/enable
 echo 1 > $pwm1/enable
+
+# Giving right to freely write in configuration files for every users
+chmod -R ugo+rw /sys/class/pwm/pwmchip0/pwm0/
+chmod -R ugo+rw /sys/class/pwm/pwmchip0/pwm1/
+
--- src/set_pwm.sh
+++ src/set_pwm.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
+set -uo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+IFS=$'\n\t'
+
 chip0=/sys/class/pwm/pwmchip0
 pwm0=$chip0/pwm0
 pwm1=$chip0/pwm1
 
-pwm0() {
-    echo $1*500000+1500000 > $pwm0/duty_cycle
-}
-
-pwm1() {
-    echo $1*500000+1500000 > $pwm1/duty_cycle
-}
-
-pwm0 $1
-pwm1 $2
+echo $1*500000+1500000 | bc -l
+echo 20000000.0  > $pwm0/duty_cycle
+#echo "$u2" > $pwm1/duty_cycle
